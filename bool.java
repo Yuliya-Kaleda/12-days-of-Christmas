80544bffe633b1812670769d65f913a95dfb0061
Yuliya-Kaleda <yulia.koleda1988@gmail.com>
2015-03-03 21:46:24 -0500

    song

---
+++ bool.java
@@ -0,0 +1,124 @@
+package nyc.c4q.yuliyakaleda;
+
+/**
+ * Created by olgakoleda on 3/3/15.
+ */
+public class bool {
+    public static void main(String[] args) {
+        String first="On the ";
+        String christmas = " of Christmas my true love sent to me";
+        String partridge="a Partridge in a Pear Tree";
+        String doves= "Two Turtle Doves";
+        String three="Three French Hens";
+        String birds="Four Calling Birds";
+        String golden_rings="Five Golden Rings";
+        String geese="Six Geese a Laying";
+        String swans="Seven Swans a Swimming";
+        String maids="Eight Maids a Miking";
+        String ladies="Nine Ladies Dancing";
+        String lords="Ten Lords a Leaping";
+        String pipers="Eleven Pipers Piping";
+        String drummers="12 Drummers Drumming";
+
+
+        System.out.println(first + "first" + christmas);
+        System.out.println(partridge + "\n");
+
+        System.out.println(first + "second" + christmas);
+        System.out.println(doves);
+        System.out.println(partridge + "\n");
+
+        System.out.println(first + "third" + christmas);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(geese);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(swans);
+        System.out.println(geese);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(maids);
+        System.out.println(swans);
+        System.out.println(geese);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(ladies);
+        System.out.println(maids);
+        System.out.println(swans);
+        System.out.println(geese);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(lords);
+        System.out.println(ladies);
+        System.out.println(maids);
+        System.out.println(swans);
+        System.out.println(geese);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(pipers);
+        System.out.println(ladies);
+        System.out.println(maids);
+        System.out.println(swans);
+        System.out.println(geese);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+
+        System.out.println(first + "forth" + christmas);
+        System.out.println(drummers);
+        System.out.println(pipers);
+        System.out.println(ladies);
+        System.out.println(maids);
+        System.out.println(swans);
+        System.out.println(geese);
+        System.out.println(golden_rings);
+        System.out.println(birds);
+        System.out.println(three);
+        System.out.println(doves);
+        System.out.println("and " + partridge + "\n");
+    }
+}
